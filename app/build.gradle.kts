e1e38f3817cc4c1742cb22f68b1734b30066f7ce
Overmort <overmort3@gmail.com>
2024-05-17 23:06:32 +0300

    1.3 Добавление lifecycle-viewmodel-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.lifecycle.viewmodel.compose)
 }
